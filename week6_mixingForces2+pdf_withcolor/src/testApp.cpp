8b4268c59d03abf4dca7fc317f0cfd657604a532
Shankari <muras074@newschool.edu>
2012-12-27 14:00:45 +0530

    week6_mixingForces2_withColor

---
+++ week6_mixingForces2+pdf_withcolor/src/testApp.cpp
@@ -0,0 +1,155 @@
+#include "testApp.h"
+
+// comparison routine for sort...
+bool comparisonFunction(  particle * a, particle * b ) { 
+	return a->pos.x < b->pos.x; 
+}	
+//--------------------------------------------------------------
+void testApp::setup(){	
+	
+	ofSetVerticalSync(true);
+	ofSetFrameRate(60);
+	ofBackground(255, 255, 255);
+	//ofSetBackgroundAuto(false);
+	scale=4;
+	image.loadImage("1.jpg");
+	image2.loadImage("1.jpg");
+	image2.setImageType(OF_IMAGE_COLOR);
+	image.setImageType(OF_IMAGE_COLOR);
+	imageWidth=image.width;
+	imageHeight=image.height;
+	
+
+
+	learnImage=true;
+	
+	
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	
+	// on every frame 
+	// we reset the forces
+	// add in any forces on the particle
+	// perfom damping and
+	// then update
+	
+	if (learnImage) {
+		
+		unsigned char* pixels=image.getPixels();
+		imagePixels.setFromPixels(image.getPixels(), image.width, image.height, 3);
+		
+		myParticles.clear();
+		for (int x = 0; x < imageWidth/scale; x++)
+			for(int y = 0; y < imageHeight/scale; y++){
+			
+				particle * p = new particle();
+				
+				p->pos.set(ofGetWidth()/2-imageWidth/2+ x*scale, ofGetHeight()/2-imageHeight/2+ y*scale);
+				p->size=scale;
+				ofColor pixelColor = imagePixels.getColor(x*scale, y*scale);
+				p->color.set(pixelColor);
+				
+				myParticles.push_back(p);
+			}
+		
+		learnImage=false;
+	}
+	
+	// sort all the particles:
+	
+	sort( myParticles.begin(), myParticles.end(), comparisonFunction );		
+	
+	
+	
+	
+	for (int i = 0; i < myParticles.size(); i++){
+		
+		ofColor particleColor=myParticles[i]->color;
+		int x=mouseX;
+		int y=mouseY;
+		float addHue=myParticles[i]->addColor(x,y,50,80);
+		float addSaturation=myParticles[i]->addColor(x,y,50,150);
+		float addBrightness=myParticles[i]->addColor(x,y,50,80);
+		
+		float hue=particleColor.getHue();
+		float saturation=particleColor.getSaturation();
+		float brightness=particleColor.getBrightness();
+		
+		
+		particleColor.setHsb(ofClamp(hue+addHue, 0, 255),ofClamp( saturation+addSaturation, 0, 255),ofClamp( brightness+addBrightness, 0, 255));
+		myParticles[i]->color.set(particleColor);
+		
+		myParticles[i]->addAttractionForce(mouseX,mouseY,20,1.0f);
+		myParticles[i]->addRepulsionForce(mouseX,mouseY,ofMap(mouseY, 0, ofGetHeight(), 1, 200, true),1.0f);
+		
+		myParticles[i]->addDampingForce();
+		myParticles[i]->update();
+		
+	}
+	
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	for (int i = 0; i < myParticles.size(); i++){
+		
+		myParticles[i]->draw();
+	}
+	
+	
+	
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){ 
+	
+	if (key == ' '){
+		VF.clear();
+	} 
+	
+	if (key=='a') {
+		learnImage=true;
+	}
+	if( key=='p'){
+			ofSetFrameRate(10);
+			ofBeginSaveScreenAsPDF(ofGetTimestampString()+".pdf", true);
+	
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased  (int key){ 
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+	
+	//VF.addOutwardCircle(mouseX, mouseY, 200, 0.005f);
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+	
+	float diffx = x - prevMouseX;
+	float diffy = y - prevMouseY;
+	
+	VF.addVectorCircle((float)x, (float)y, diffx*0.3, diffy*0.3, 60, 0.3f);
+	
+	prevMouseX = x;
+	prevMouseY = y;
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+	prevMouseX = x; 
+	prevMouseY = y;
+	
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(){
+}
